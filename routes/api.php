b5574798dbe60dbf985711e376ea56aef0f8b196
vikas <kushwahavikas1993@gmail.com>
2018-09-14 16:45:47 +0530

    Add passport api

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,14 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::post('login', 'API\UserController@login');
+Route::post('register', 'API\UserController@register');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => 'auth:api'], function(){
+	Route::post('details', 'API\UserController@details');
+	
+	});
